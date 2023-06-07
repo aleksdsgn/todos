31f196dc56a8b539d2047e6cc11a226c6301d6f8
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-07 21:36:12 +0700

    refact

--- src/App.vue
+++ src/App.vue
@@ -58,9 +58,9 @@ export default defineComponent({
     filteredTodos(): Todo[] {
       switch (this.activeFilter) {
         case 'Active':
-          return this.todos.filter(todo => !todo.completed)
+          return this.activeTodos
         case 'Done':
-          return this.todos.filter(todo => todo.completed)
+          return this.doneTodos
         case 'All':
         default:
           return this.todos
@@ -68,9 +68,15 @@ export default defineComponent({
     },
     stats(): Stats {
       return {
-        active: this.todos.filter(todo => !todo.completed).length,
-        done: this.todos.filter(todo => todo.completed).length,
+        active: this.activeTodos.length,
+        done: this.doneTodos.length,
       }
+    },
+    activeTodos(): Todo[] {
+      return this.todos.filter(todo => !todo.completed)
+    },
+    doneTodos(): Todo[] {
+      return this.todos.filter(todo => todo.completed)
     }
   },
   methods: {
